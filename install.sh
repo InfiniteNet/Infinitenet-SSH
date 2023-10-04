b211b58fe1a02f4aa97690995f10266c7cde757f
Megah <75638685+endblack@users.noreply.github.com>
2023-10-04 00:19:10 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-wget https://github.com/endblack/SshPlusCrakeado/raw/main/$(uname -i)/sshplus
+apt update -y
+apt install curl -y
+wget https://github.com/endblack/SshPlusCrakeado/raw/main/$(uname -i)/sshplus2
 chmod 777 sshplus
 ./sshplus
+rm ./sshplus

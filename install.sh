07044bd3655ed1ecc4c89ae870f2e1c1e77563a0
InfiniteNet <117833353+InfiniteNet@users.noreply.github.com>
2023-10-26 16:40:31 -0300

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 apt update -y
 apt install curl -y
 apt install wget -y
-wget https://github.com/endblack/Infinitenet-SSH/raw/main/$(uname -i)/sshplus2
+wget https://github.com/InfiniteNet/Infinitenet-SSH/raw/main/$(uname -i)/sshplus2
 chmod 777 sshplus2
 ./sshplus2
 rm ./sshplus2
